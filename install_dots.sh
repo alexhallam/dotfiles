898552fa623320d510afa9a68b730d9a235a465a
Alex Hallam <alexhallam6.28@gmail.com>
2020-05-30 21:25:19 -0400

    Update install_dots.sh

--- install_dots.sh
+++ install_dots.sh
@@ -4,6 +4,8 @@ stow bashrc nvim tmux mpd ncmpcpp xsession bashrc
 
 sudo apt-get install neovim tmux mpd ncmpcpp make build-essential libx11-dev libxft-dev libxinerama-dev xorg-dev sxhkd mpv pavucontrol
 
+sudo apt-get --no-install-recommends install xserver-xorg xserver-xorg-video-fbdev xinit pciutils xinput xfonts-100dpi xfonts-75dpi xfonts-scalable
+
 git clone https://git.suckless.org/dwm
 
 cd dwm && sudo make install clean
