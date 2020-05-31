a89f585b9f0fb0a767932f4b61253bff759f83d7
Alex Hallam <alex.hallam@cfacorp.com>
2020-05-30 20:34:18 -0400

    add bash

---
+++ install_dots.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# License: GNU GPLv3
+
+sudo apt install  git stow
+git clone https://github.com/alexhallam/dotfiles.git
+
+cd dotfiles
+
+stow bashrc nvim tmux mpd ncmpcpp xsession bashrc 
+
+sudo apt-get install neovim tmux mpd ncmpcpp make build-essential libx11-dev libxft-dev libxinerama-dev xorg-dev sxhkd mpv pavucontrol
+
+git clone https://git.suckless.org/dwm
+
+cd dwm && sudo make install clean
+
+git clone https://git.suckless.org/st
+
+cd st && sudo make install clean
